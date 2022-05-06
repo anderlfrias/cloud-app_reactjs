b6609b2f6a4e1e00408dc919f997a7a35a8bf2e7
anderlfrias <anderl.friaas@gmail.com>
2022-05-06 15:51:44 -0400

    Componente AllInOne listo

---
+++ src/components/AllInOne.jsx
@@ -0,0 +1,133 @@
+import React from 'react'
+import { CheckIcon } from '@heroicons/react/outline'
+
+function AllInOne() {
+    return (
+        <div className='w-full my-32'>
+            <div className='max-w-[1240px] mx-auto px-2'>
+                <h2 className='text-5xl font-bold text-center'>All-In-One Platform</h2>
+                <p className='text-2xl py-8 text-gray-500 text-center'>
+                    All-in-one platform is a web application that allows users to
+                    manage their personal finances. It is a web application that
+                    allows users to manage their personal finances. It is a web
+                </p>
+            </div>
+
+            <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4'>
+                <div className='flex'>
+                    <div>
+                        <CheckIcon className='w-7 mr-4 text-green-600'/>
+                    </div>
+                    <div>
+                        <h3 className='font-bold text-lg'>Notificatins</h3>
+                        <p className='text-lg pt-2 pb-4'>
+                            All-in-one platform is a web application that allows users to
+                            manage their personal finances. It is a web application that
+                            allows users to manage their personal finances. It is a web
+                        </p>
+                    </div>
+                </div>
+
+                <div className='flex'>
+                    <div>
+                        <CheckIcon className='w-7 mr-4 text-green-600'/>
+                    </div>
+                    <div>
+                        <h3 className='font-bold text-lg'>Notificatins</h3>
+                        <p className='text-lg pt-2 pb-4'>
+                            All-in-one platform is a web application that allows users to
+                            manage their personal finances. It is a web application that
+                            allows users to manage their personal finances. It is a web
+                        </p>
+                    </div>
+                </div>
+
+                <div className='flex'>
+                    <div>
+                        <CheckIcon className='w-7 mr-4 text-green-600'/>
+                    </div>
+                    <div>
+                        <h3 className='font-bold text-lg'>Notificatins</h3>
+                        <p className='text-lg pt-2 pb-4'>
+                            All-in-one platform is a web application that allows users to
+                            manage their personal finances. It is a web application that
+                            allows users to manage their personal finances. It is a web
+                        </p>
+                    </div>
+                </div>
+
+                <div className='flex'>
+                    <div>
+                        <CheckIcon className='w-7 mr-4 text-green-600'/>
+                    </div>
+                    <div>
+                        <h3 className='font-bold text-lg'>Notificatins</h3>
+                        <p className='text-lg pt-2 pb-4'>
+                            All-in-one platform is a web application that allows users to
+                            manage their personal finances. It is a web application that
+                            allows users to manage their personal finances. It is a web
+                        </p>
+                    </div>
+                </div>
+
+                <div className='flex'>
+                    <div>
+                        <CheckIcon className='w-7 mr-4 text-green-600'/>
+                    </div>
+                    <div>
+                        <h3 className='font-bold text-lg'>Notificatins</h3>
+                        <p className='text-lg pt-2 pb-4'>
+                            All-in-one platform is a web application that allows users to
+                            manage their personal finances. It is a web application that
+                            allows users to manage their personal finances. It is a web
+                        </p>
+                    </div>
+                </div>
+
+                <div className='flex'>
+                    <div>
+                        <CheckIcon className='w-7 mr-4 text-green-600'/>
+                    </div>
+                    <div>
+                        <h3 className='font-bold text-lg'>Notificatins</h3>
+                        <p className='text-lg pt-2 pb-4'>
+                            All-in-one platform is a web application that allows users to
+                            manage their personal finances. It is a web application that
+                            allows users to manage their personal finances. It is a web
+                        </p>
+                    </div>
+                </div>
+
+                <div className='flex'>
+                    <div>
+                        <CheckIcon className='w-7 mr-4 text-green-600'/>
+                    </div>
+                    <div>
+                        <h3 className='font-bold text-lg'>Notificatins</h3>
+                        <p className='text-lg pt-2 pb-4'>
+                            All-in-one platform is a web application that allows users to
+                            manage their personal finances. It is a web application that
+                            allows users to manage their personal finances. It is a web
+                        </p>
+                    </div>
+                </div>
+
+                <div className='flex'>
+                    <div>
+                        <CheckIcon className='w-7 mr-4 text-green-600'/>
+                    </div>
+                    <div>
+                        <h3 className='font-bold text-lg'>Notificatins</h3>
+                        <p className='text-lg pt-2 pb-4'>
+                            All-in-one platform is a web application that allows users to
+                            manage their personal finances. It is a web application that
+                            allows users to manage their personal finances. It is a web
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AllInOne
