64d1e68fff3134cef64dda52a27e7d6a05e42669
anderlfrias <anderl.friaas@gmail.com>
2022-05-09 14:38:21 -0400

    componente footer listo + detalles finales de la app

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,24 @@
 import React, {useState} from 'react';
 import {MenuIcon, XIcon} from '@heroicons/react/outline';
+import { Link, animateScroll as scroll, } from 'react-scroll'
 
 export const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
     const handleClick = () => setIsOpen(!isOpen);
 
+    const handleClose = () => setIsOpen(false);
+
     return (
         <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
             <div className='px-2 flex justify-between items-center w-full h-full'>
-                <div className='flex items-center'>
+                <div className='flex items-center '>
                     <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>BRAND.</h1>
                     <ul className='hidden md:flex'>
-                        <li className=''>Home</li>
-                        <li className=''>About</li>
-                        <li className=''>Support</li>
-                        <li className=''>Platform</li>
-                        <li className=''>Pricing</li>
+                        <li className='cursor-default'><Link onClick={handleClose} to='home' smooth={true} duration={500}>Home</Link></li>
+                        <li className='cursor-default'><Link onClick={handleClose} to='about' smooth={true} offset={-200} duration={500}>About</Link></li>
+                        <li className='cursor-default'><Link onClick={handleClose} to='support' smooth={true} offset={-50} duration={500}>Support</Link></li>
+                        <li className='cursor-default'><Link onClick={handleClose} to='platform' smooth={true} offset={-100} duration={500}>Platform</Link></li>
+                        <li className='cursor-default'><Link onClick={handleClose} to='pricing' smooth={true} offset={-50} duration={500}>Pricing</Link></li>
                     </ul>
                 </div>
                 <div className='hidden md:flex pr-4'>
@@ -32,11 +35,11 @@ export const Navbar = () => {
             </div>
 
             <ul className={!isOpen ? 'hidden' : 'absolute bg-zinc-200 w-full px-8'}>
-                <li className='border-b border-zinc-300 w-full'>Home</li>
-                <li className='border-b border-zinc-300 w-full'>About</li>
-                <li className='border-b border-zinc-300 w-full'>Support</li>
-                <li className='border-b border-zinc-300 w-full'>Platform</li>
-                <li className='border-b border-zinc-300 w-full'>Pricing</li>
+                <li className='border-b border-zinc-300 w-full'><Link onClick={handleClose} to='home' smooth={true} duration={500}>Home</Link></li>
+                <li className='border-b border-zinc-300 w-full'><Link onClick={handleClose} to='about' smooth={true} offset={-200} duration={500}>About</Link></li>
+                <li className='border-b border-zinc-300 w-full'><Link onClick={handleClose} to='support' smooth={true} offset={-50} duration={500}>Support</Link></li>
+                <li className='border-b border-zinc-300 w-full'><Link onClick={handleClose} to='platform' smooth={true} offset={-100} duration={500}>Platform</Link></li>
+                <li className='border-b border-zinc-300 w-full'><Link onClick={handleClose} to='pricing' smooth={true} offset={-50} duration={500}>Pricing</Link></li>
                 <div className='flex flex-col my-4'>
                     <button className='bg-transparent text-indigo-600 px-8 py-3 mb-4'>
                         Sing Up
